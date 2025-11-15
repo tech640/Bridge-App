fbd1f0ba988d69e83e25a575c4e07479d610277e
hadeelBanimattar <hadeel.bm@nadsoft.net>
2025-11-15 20:37:56 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'screens/home.dart'; // تأكد إنو اسم الملف صح
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(), // استدعاء الصفحة الرئيسية
+    );
+  }
+}
